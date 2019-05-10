50e41d9155520c49696d05eed0f3c389076ac5a6
c.perrot <perrot.com@gmail.com>
2019-05-10 15:24:09 +0200

    First working version of the summary

--- routes/userSummary.js
+++ routes/userSummary.js
@@ -14,15 +14,28 @@ router.get('/', async(req, res) => {
         .populate(projectPopulateQuery);
 
     let taskPopulateQuery = [{path : 'project', select : 'name'},
-        {path : 'status', select : 'name -_id'}];
+        {path : 'status', select : 'name'},
+        {path : 'assignee', select : 'name firstname'}];
     let tasks = await Task.find({assignee : req.session.userId})
         .populate(taskPopulateQuery);
 
     let finishedTasks = await Task.find({assignee : req.session.userId, status : '5cac811bf91f9f2030bd6dc7'})
         .populate(taskPopulateQuery);
 
-    console.log(projects);
-    res.render('userSummary',{projects : projects, tasks : tasks, finishedTasks : finishedTasks});
+    let projectTasksArray = [];
+
+    for(let p in projects){
+        let project = projects[p];
+
+        let projectTasks = await Task.find({project : project._id})
+            .populate(taskPopulateQuery);
+
+        projectTasksArray.push({projectName : project.name, tasks : projectTasks});
+    }
+
+    res.render('userSummary',{projects : projects,
+        tasks : tasks, finishedTasks : finishedTasks,
+        projectTasksArray : projectTasksArray});
 });
 
 module.exports = router;
