c62365814e1e939f9a8ec4bc925d205a7a23b8da
EduardoD <edadaltocg@gmail.com>
2019-05-16 10:36:07 +0200

    Final Changes before presentation

--- routes/export.js
+++ routes/export.js
@@ -1,48 +1,68 @@
 const express = require('express');
-
 const Project = require('../models/projectModel');
 const Task = require('../models/taskModel');
-
-const fs = require('fs');
-
 const router = express.Router();
-
 var exportMethods = require('../public/javascripts/exportMethods');
 
+/*Get page where the possible file formats are proposed*/
 router.get('/',async (req,res) => {
 
-    req.session.zipContent = [];
-    req.session.exportProjects = [];
+    req.session.zipContent = []; //initialize zipContent for storing the content to be sent
+    req.session.exportProjects = []; //initialize exportProjects for storing the projects to be exported
     var formats = [
-        {_id:'csvFile',name:'CSV'},
-        {_id:'xmlFile',name:'XML'},
+        {_id:'csvFile',name:'  CSV'},
+        {_id:'xmlFile',name:'  XML'},
         {_id:'jsonFile',name:'JSON'},
-        {_id:'excelFile',name:'MS EXCEL'}
-        ];
+        ]; // three formats provided
 
-    res.render('exportForm',{choices:formats,object:'dataFormat',firstName: req.session.firstname, lastName: req.session.name});
+    res.render('exportForm',{choices:formats,title:'dataFormat',aim:'projects',firstName: req.session.firstname, lastName: req.session.name});
 
 });
 
-router.post('/dataFormat',async (req,res) => {
+/*POST lists all the available projects to be exported*/
+router.post('/projects',async (req,res) => {
 
-    var formatsArray = Object.keys(req.body);
-    req.session.dataFormatExport = formatsArray;
+    var formatsArray = Object.keys(req.body);//the formats chosen by user
+    req.session.dataFormatExport = formatsArray;//store the formats in the session
     var filterQuery = {};
     var projectsData;
     if (req.session.role !== "admin") {
         filterQuery = {members:req.session.userId};
-    }
-
+    } // if the user is not admin, he will only see the projects he participated
     projectsData = await Project.find(filterQuery)
         .catch((mongoError) => res.render('error', {error: mongoError}));
-    res.render('exportForm',{choices:projectsData,object:'projects',firstName: req.session.firstname, lastName: req.session.name});
+    res.render('exportForm',{choices:projectsData,title:'projects',aim:'tasks',firstName: req.session.firstname, lastName: req.session.name});
 
 });
 
+
+/*POST lists all the tasks belonging to the projects chosen bu the user*/
 router.post('/tasks',async (req,res) => {
-    /*export all the tasks assigned to him*/
-    console.log(req.session.dataFormatExport);
+
+    var filterQuery = {project:Object.keys(req.body)};//the projects chosen by the user
+    req.session.exportProjects = Object.keys(req.body); // store the projects in the session
+    var tasksData;
+    for (var i = 0;i<req.session.dataFormatExport.length;i++) {
+
+        switch (req.session.dataFormatExport[i]) {
+            case 'csvFile':
+                await exportMethods.exportProjectsCSV(req,res);
+                break;
+            default:
+                break;
+        }
+    }// if CSV is chosen, the files concerning basic information of projects will be firstly exported
+    if (req.session.role !== "admin") {
+        filterQuery.assignee = req.session.userId;
+    }// if the user is not admin, he will only see the tasks assigned to him
+    tasksData = await Task.find(filterQuery)
+        .catch((mongoError) => res.render('error',{error:mongoError}));
+    res.render('exportForm',{choices:tasksData,title:'tasks',aim:'download',firstName: req.session.firstname, lastName: req.session.name});
+});
+
+/*POST send a zip file containing the files demanded*/
+router.post('/download',async (req,res) => {
+
     for (var i = 0;i<req.session.dataFormatExport.length;i++) {
 
         switch (req.session.dataFormatExport[i]) {
@@ -59,39 +79,13 @@ router.post('/tasks',async (req,res) => {
                 res.redirect('/projects');
                 return ;
         }
-    }
+    }//for each selected format, export the data in the corresponding files
 
 
     console.log("----------compress---------");
-    //console.log(req.session.zipContent);
-    res.zip({files:req.session.zipContent, filename:'myfile.zip'});
+    res.zip({files:req.session.zipContent, filename:'myfile.zip'});//put all the files in a zip file and send it
     req.session.zipContent = [];
     console.log("----------fini---------");
-});
-
-/*export all the projects he participates*/
-router.post('/projects',async (req,res) => {
-
-    var filterQuery = {project:Object.keys(req.body)};
-    req.session.exportProjects = Object.keys(req.body);
-    var tasksData;
-    for (var i = 0;i<req.session.dataFormatExport.length;i++) {
-
-        switch (req.session.dataFormatExport[i]) {
-            case 'csvFile':
-                await exportMethods.exportProjectsCSV(req,res);
-                break;
-            default:
-                break;
-        }
-    }
-
-    if (req.session.role !== "admin") {
-        filterQuery.assignee = req.session.userId;
-    }
-    tasksData = await Task.find(filterQuery)
-        .catch((mongoError) => res.render('error',{error:mongoError}));
-    res.render('exportForm',{choices:tasksData,object:'tasks',firstName: req.session.firstname, lastName: req.session.name});
 });
 
 module.exports = router;
